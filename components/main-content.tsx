95993856c0752b1dd3f4bfddad05797b575ff622
mdcartel <markdavid1311811@gmail.com>
2025-08-08 22:47:12 +0300

    changes

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -1,18 +1,10 @@
 "use client";
 import React from 'react'
-import Sidebar from './sidebar'
-import Topbar from './topbar'
 
 export default function MainContent({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col min-h-screen">
-        <Topbar />
-        <main className="flex-1 flex flex-col items-center justify-center p-8">
-          {children}
-        </main>
-      </div>
+    <div className="min-h-screen">
+      {children}
     </div>
   )
 } 
